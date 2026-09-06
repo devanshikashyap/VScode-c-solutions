2c92cd109d1414e66c8cd7e98c6134ae76f52580
devanshikashyap007 <devanshi.kashyap007@gmail.com>
2026-09-06 14:59:06 +0530

    Add new C code

---
+++ radix_sort.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+// Find the largest element
+int getMax(int arr[], int n) {
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+// Counting sort according to each digit
+void countingSort(int arr[], int n, int place) {
+    int output[100];
+    int count[10] = {0};
+
+    // Count occurrences of each digit
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / place) % 10;
+        count[digit]++;
+    }
+
+    // Calculate positions
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    // Build output array
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / place) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+    // Copy back to original array
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+// Radix Sort
+void radixSort(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    // Sort by units, tens, hundreds, ...
+    for (int place = 1; max / place > 0; place *= 10) {
+        countingSort(arr, n, place);
+    }
+}
+
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    radixSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
