8f1bf605d74605f1c9f812b475d380d892490d06
devanshikashyap007 <devanshi.kashyap007@gmail.com>
2026-09-14 20:32:42 +0530

    Add new C code

---
+++ circular_LL_deletion.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+// Insert node at the end
+void insert()
+{
+    int value;
+    struct Node *newNode, *temp;
+
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    printf("Enter value: ");
+    scanf("%d", &value);
+
+    newNode->data = value;
+
+    if (head == NULL)
+    {
+        head = newNode;
+        newNode->next = head;
+    }
+    else
+    {
+        temp = head;
+
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+        newNode->next = head;
+    }
+}
+
+// Delete first node
+void deletion()
+{
+    struct Node *temp, *last;
+
+    if (head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    // If only one node exists
+    if (head->next == head)
+    {
+        free(head);
+        head = NULL;
+        printf("Node deleted successfully.\n");
+        return;
+    }
+
+    // Find last node
+    last = head;
+
+    while (last->next != head)
+    {
+        last = last->next;
+    }
+
+    temp = head;
+    head = head->next;
+    last->next = head;
+
+    free(temp);
+
+    printf("Node deleted successfully.\n");
+}
+
+// Display circular linked list
+void display()
+{
+    struct Node *temp;
+
+    if (head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    temp = head;
+
+    printf("Circular Linked List: ");
+
+    do
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    while (temp != head);
+
+    printf("HEAD\n");
+}
+
+int main()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n--- Circular Linked List ---\n");
+        printf("1. Insertion\n");
+        printf("2. Deletion\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                insert();
+                break;
+
+            case 2:
+                deletion();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                exit(0);
+
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
+
+    return 0;
+}
